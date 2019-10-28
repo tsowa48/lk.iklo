381cc0e0614a32fc93603e90c6a803ce913ca85b
A G <47570228+tsowa48@users.noreply.github.com>
2019-10-28 10:37:20 +0300

    New year fix

--- test/reports/r2.php
+++ test/reports/r2.php
@@ -7,7 +7,7 @@
   echo '<div class=\'panel-heading\' style=\'font-size:15px;text-align:center;\'>';
   if(isset($_GET['tid'])) {
     $tid = (int)$_GET['tid'];
-    $t = pg_fetch_row(pg_query($_SESSION['psql'], 'select T.text, V.name from test T, vote V where V.id=T.vid and T.id='.$tid.';'));
+    $t = pg_fetch_row(pg_query($_SESSION['psql'], 'select T.text, V.name from test T, vote V where V.year='.date('Y').' and V.id=T.vid and T.id='.$tid.';'));
     echo $t[0].'<br>('.$t[1].')</div>';
     $result = pg_query($_SESSION['psql'], 'select U.id, U.fio, K.name, R.ball from users U, result R, test T, komission K where K.id=U.kid and T.passball>R.ball and R.tid=T.id and R.uid=U.id and R.tid='.$tid.' order by K.name, U.fio;');
     if(pg_num_rows($result) > 0) {
@@ -21,7 +21,7 @@
       echo '<div class=\'list-group\'><div class=\'list-group-item text-center\'>Список пуст</div></div>';
     }
   } else { // Список тем тестов
-    $tests = pg_query($_SESSION['psql'], 'select T.id, T.text, V.name from test T, vote V, klass K where T.vid=V.id and K.tid=T.id and (V.day+'.$daysAfterVote.') > '.(date('z') + 1).' group by 1,2,3 order by V.name, T.text;');
+    $tests = pg_query($_SESSION['psql'], 'select T.id, T.text, V.name from test T, vote V, klass K where V.year='.date('Y').' and T.vid=V.id and K.tid=T.id and (V.day+'.$daysAfterVote.') > '.(date('z') + 1).' group by 1,2,3 order by V.name, T.text;');
     echo '<b>Тема для отчета:</b></div>';
     echo '<div class=\'list-group\'>';
     while($t = pg_fetch_row($tests)) {
